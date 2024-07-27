76856bee0cb73c276b725ec28d7a9ce15a8e9e4c
root <root@LAPTOP-A9QB1M2U.localdomain>
2024-07-26 23:05:08 -0300

    commit editado

---
+++ primergit2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main(){
+	printf(" touch crea un archivo
+		vim lo edita 
+		mkdir crea una carpeta
+		ls muestra los archivos de la carpeta
+		cd cambia la direccion de la carpeta
+		clear borra el texto
+		git init pone a git en la carpeta a utilizar
+		git add aniade un archivo a los que se van a usar
+		git commit -m <mensaje>  guarda el archivo git
+		")
+	return 0;
+}
